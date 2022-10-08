f23396c5baba8b1b63326b8d02e74852642b9a09
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-10-08 21:39:44 +0800

    Section 4.45 Configure AutoMapper

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,7 +34,7 @@ namespace API.Controllers
 
             var product = await _productRepo.GetEntityWithSpec(spec);
 
-            return _mapper.Map<Product, ProductToReturnDto>(product);
+            return Ok(_mapper.Map<Product, ProductToReturnDto>(product));
 
            
         }
@@ -46,16 +46,12 @@ namespace API.Controllers
 
             var products = await _productRepo.ListAsync(spec);
 
-            return products.Select(product=> new ProductToReturnDto{
-                Id = product.Id,
-                Name = product.Name,
-                Description = product.Description,
-                PictureUrl = product.PictureUrl,
-                Price = product.Price,
-                ProductBrand = product.ProductBrand.Name,
-                ProductType = product.ProductType.Name
-            }).ToList();
+            return Ok(
 
+                _mapper.Map<IReadOnlyList<Product> , IReadOnlyList<ProductToReturnDto>>(products)
+            );
+
+          
         }
         [HttpGet("brands")]
 
