842f87688ea3e8bad68e9d329191f1f774def687
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-11-08 07:50:25 +0800

    Section 5.52 Adding a not found endpoint err hndlr

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,12 +34,17 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+               // app.UseSwagger();
+               // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseStaticFiles();
