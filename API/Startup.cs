462adb838860e259490c3a468a68055a44912251
Jeffrey Trajano <jeffrey.trajano@irely.com>
2022-11-08 08:12:02 +0800

    Section 5.53 Creating Exception handler middleware

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Core.Interfaces;
 using API.Helpers;
+using API.Middleware;
 
 namespace API
 {
@@ -35,13 +36,13 @@ namespace API
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
-
+            app.UseMiddleware<ExceptionMiddleware>();
 
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-               // app.UseSwagger();
-               // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+                //app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
