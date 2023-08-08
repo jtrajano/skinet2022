261a025af391a170b531ffb5f4bf4dff4b46e1c7
Jeffrey Trajano <jeffrey.trajano@irely.com>
2023-08-08 10:24:45 +0800

    Section 9.101 Making Pagination Header a component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,17 +1,21 @@
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginationHeaderComponent } from './pagination-header/pagination-header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PaginationHeaderComponent
+  ],
   imports: [
     CommonModule,
     PaginationModule.forRoot() // singleton
   ],
   exports:[
-    PaginationModule
+    PaginationModule,
+    PaginationHeaderComponent
   ]
 })
 export class SharedModule { }
