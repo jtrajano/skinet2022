3452ef47ffd60749fa3f49d0dfc40bd3d8fb444a
jtrajano <jlt212000@yahoo.com>
2023-09-15 09:27:17 +0800

    Section 10.111 Getting Individual Product

--- client/src/app/shop/product-details.component.ts
+++ client/src/app/shop/product-details.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShopService } from './shop.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../shared/models/product';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  product? : Product;
+  /**
+   *
+   */
+  constructor(private shopService: ShopService, private activatedRouted: ActivatedRoute) {
+    
+    
+  }
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+  loadProduct() {
+    const id = this.activatedRouted.snapshot.paramMap.get('id');
+
+    if(id) 
+      this.shopService.getProduct(+id).subscribe({
+        next: product => this.product = product,
+        error: error=>console.log(error)
+      })
+  }
+  
 
 }
