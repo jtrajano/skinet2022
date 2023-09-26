988c6a7d23ea1f6e733da5224dbd804c74d31ce9
jtrajano <jlt212000@yahoo.com>
2023-09-26 15:35:19 +0800

    Section 11.119  Adding toastr notif

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -8,11 +8,12 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private toastr: ToastrService) {
      
   }
 
@@ -22,6 +23,14 @@ export class ErrorInterceptor implements HttpInterceptor {
 
         if(error){
           if(error.status === 400)
+          {
+            this.toastr.error(error.error.message, error.status.toString());
+          }
+          if(error.status === 401)
+          {
+            this.toastr.error(error.error.message, error.status.toString());
+          }
+          if(error.status === 404)
             this.router.navigateByUrl('/not-found');
           if(error.status === 500)
             this.router.navigateByUrl('/server-error');
